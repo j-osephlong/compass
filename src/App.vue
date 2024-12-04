0a8c669dd42123696f568a7fcd74d5f51d783b25
Joseph Long <jlong.anthony@live.ca>
2024-12-04 12:59:11 -0400

    Try webkit prop

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
 <script setup lang="ts">
 import { useDeviceOrientation } from '@vueuse/core';
-import { computed, onMounted, useTemplateRef, watch } from 'vue';
-
-const orientation = useDeviceOrientation()
+import { computed, onMounted, ref, useTemplateRef, watch } from 'vue';
 
 const north = useTemplateRef("north")
 
-const alphaCorrected = computed(() => (360 - (orientation.alpha.value ?? 0)))
+const direction = ref<number>()
+const lastEvent = ref<string>()
 
-watch(alphaCorrected, () => {
+watch(direction, () => {
   if (north.value) {
-    north.value.style.rotate = `${(360 - (alphaCorrected.value ?? 0)).toFixed(2)}deg`
+    north.value.style.rotate = `${(360 - (direction.value ?? 0)).toFixed(2)}deg`
   }
 }) 
+
+function addEvent() {
+  window.addEventListener('deviceorientation', (e) => {
+      direction.value = 360 - (e.alpha ?? 0)
+      if ("webkitCompassHeading" in e) {
+        direction.value = e.webkitCompassHeading as number ?? 0
+      }
+      lastEvent.value = JSON.stringify(e)
+  })
+}
 
 function requestOrientationPermission(){
     if ( typeof( DeviceOrientationEvent ) !== "undefined" && "requestPermission" in DeviceOrientationEvent && typeof( DeviceOrientationEvent.requestPermission ) === "function" )
@@ -20,12 +29,12 @@ function requestOrientationPermission(){
       DeviceOrientationEvent.requestPermission()
         .then((response: any) => {
             if (response == 'granted') {
-                window.addEventListener('deviceorientation', (e) => {
-                    // do something with e
-                })
+                addEvent()
             }
         })
         .catch(console.error)
+    } else {
+      addEvent()
     }
 }
 
@@ -37,11 +46,12 @@ function requestOrientationPermission(){
   </header>
 
   <main>
-    <h2>{{ alphaCorrected?.toFixed(1) }}</h2>
+    <h2>{{ direction?.toFixed(1) }}</h2>
     N
     <div id="north" ref="north">⬆️</div>
     S
     <button style="margin-top: 1rem;" @click="requestOrientationPermission">Give Permissions</button>
+    <p>{{ lastEvent }}</p>
   </main>
 </template>
 
